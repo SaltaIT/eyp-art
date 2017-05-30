32fbafb47a62426eb1ba19a759cd77808d95a1cc
Jordi Prats <jprats@systemadmin.es>
2017-05-30 13:04:00 +0200

    centos 7 only

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,7 +6,6 @@ class art::params {
   $gpg_art_url='https://www.atomicorp.com/RPM-GPG-KEY.art.txt'
   $gpg_atomicorp_url='https://www.atomicorp.com/RPM-GPG-KEY.atomicorp.txt'
 
-
   case $::osfamily
   {
     'redhat':
@@ -20,27 +19,27 @@ class art::params {
         default: { fail("Unsupported RHEL/CentOS version! - ${::operatingsystemrelease}")  }
       }
     }
-    'Debian':
-    {
-      case $::operatingsystem
-      {
-        'Ubuntu':
-        {
-          case $::operatingsystemrelease
-          {
-            /^14.*$/:
-            {
-            }
-            /^16.*$/:
-            {
-            }
-            default: { fail("Unsupported Ubuntu version! - ${::operatingsystemrelease}")  }
-          }
-        }
-        'Debian': { fail('Unsupported')  }
-        default: { fail('Unsupported Debian flavour!')  }
-      }
-    }
+    # 'Debian':
+    # {
+    #   case $::operatingsystem
+    #   {
+    #     'Ubuntu':
+    #     {
+    #       case $::operatingsystemrelease
+    #       {
+    #         /^14.*$/:
+    #         {
+    #         }
+    #         /^16.*$/:
+    #         {
+    #         }
+    #         default: { fail("Unsupported Ubuntu version! - ${::operatingsystemrelease}")  }
+    #       }
+    #     }
+    #     'Debian': { fail('Unsupported')  }
+    #     default: { fail('Unsupported Debian flavour!')  }
+    #   }
+    # }
     default: { fail('Unsupported OS!')  }
   }
 }
