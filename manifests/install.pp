672a3952d8497371de6339e74a883197adfcd53e
Jordi Prats <jordi.prats@gmail.com>
2019-05-07 10:12:03 +0200

    lint

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,9 +4,9 @@ class art::install inherits art {
 		path => '/bin:/sbin:/usr/bin:/usr/sbin',
 	}
 
-  exec { "art mkdir p ${gpgkeydir}":
-    command => "mkdir -p ${gpgkeydir}",
-    creates => $gpgkeydir,
+  exec { "art mkdir p ${art::gpgkeydir}":
+    command => "mkdir -p ${art::gpgkeydir}",
+    creates => $art::gpgkeydir,
   }
 
   exec { 'art which wget':
@@ -17,15 +17,15 @@ class art::install inherits art {
   # $gpg_art_url
   exec { 'wget art gpg':
     command => "wget ${$art::params::gpg_art_url} -O ${gpgkeydir}/RPM-GPG-KEY.art.txt",
-    creates => "${gpgkeydir}/RPM-GPG-KEY.art.txt",
-    require => Exec[ [ 'art which wget', "art mkdir p ${gpgkeydir}" ] ],
+    creates => "${art::gpgkeydir}/RPM-GPG-KEY.art.txt",
+    require => Exec[ [ 'art which wget', "art mkdir p ${art::gpgkeydir}" ] ],
   }
 
   # $gpg_atomicorp_url
   exec { 'wget atomicorp gpg':
-    command => "wget ${$art::params::gpg_atomicorp_url} -O ${gpgkeydir}/RPM-GPG-KEY.atomicorp.txt",
-    creates => "${gpgkeydir}/RPM-GPG-KEY.atomicorp.txt",
-    require => Exec[ [ 'art which wget', "art mkdir p ${gpgkeydir}" ] ],
+    command => "wget ${$art::params::gpg_atomicorp_url} -O ${art::gpgkeydir}/RPM-GPG-KEY.atomicorp.txt",
+    creates => "${art::gpgkeydir}/RPM-GPG-KEY.atomicorp.txt",
+    require => Exec[ [ 'art which wget', "art mkdir p ${art::gpgkeydir}" ] ],
   }
 
 
